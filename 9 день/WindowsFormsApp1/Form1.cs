7e94831110ecf56158845925ada29c4732663584
Yujiriy <yamolodec5@gmail.com>
2024-04-17 10:03:30 +0300

    9 день

---
+++ 9 день/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Series<Function> series;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Function ellipse = new Ellipse();
+            Function parabola = new Parabola();
+            Function hyperbola = new Hyperbola();
+            Function[] functions = { ellipse, hyperbola, parabola };
+
+            series = new Series<Function>(functions);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x;
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            if (!double.TryParse(textBox1.Text, NumberStyles.Float, cultureInfo, out x))
+            {
+                errorProvider1.SetError(textBox1, "Некорректно введено");
+            }
+            else
+            {
+                errorProvider1.Clear();
+                string results = series.CalculateAndDisplayResults(x);
+                richTextBox1.Text = results;
+            }
+        }
+    }
+}
---
+++ 9 день/WindowsFormsApp1/Function.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Series<T> where T : Function
+    {
+        private T[] functions;
+
+        public Series(T[] functions)
+        {
+            this.functions = functions;
+        }
+
+        public string CalculateAndDisplayResults(double x)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T function in functions)
+            {
+                double result = function.Calculate(x);
+                string display = function.DisplayResult();
+                sb.AppendLine(display);
+            }
+            return sb.ToString();
+        }
+    }
+    abstract class Function
+    {
+        public virtual double Calculate(double x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string DisplayResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Ellipse : Function
+    {
+        private double result;
+
+        public override double Calculate(double x)
+        {
+            result = Math.Sqrt(1 - Math.Pow(x, 2));
+            return result;
+        }
+
+        public override string DisplayResult()
+        {
+            return "Значение функции для эллипса: " + result;
+        }
+    }
+
+    class Hyperbola : Function
+    {
+        private double result;
+
+        public override double Calculate(double x)
+        {
+            result = (x == 0 ? double.PositiveInfinity : 1 / x);
+            return result;
+        }
+
+        public override string DisplayResult()
+        {
+            return "Значение функции для гиперболы: " + result;
+        }
+    }
+
+    class Parabola : Function
+    {
+        private double result;
+
+        public override double Calculate(double x)
+        {
+            result = Math.Pow(x, 2);
+            return result; 
+        }
+
+        public override string DisplayResult()
+        {
+            return "Значение функции для параболы: " + result;
+        }
+    }
+
+}
