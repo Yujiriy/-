53d18b07c61b27c6190a0f5c7334f2efd12c51d1
Yujiriy <yamolodec5@gmail.com>
2024-05-15 09:05:17 +0300

    23 день (2.5)

---
+++ 23 день/try/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace @try
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.employessEntitiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employessEntitiesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-6, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1132, 453);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(134, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(710, 28);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(134, 22);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(867, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Поиск";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // employessEntitiesBindingSource
+            // 
+            this.employessEntitiesBindingSource.DataSource = typeof(@try.employessEntities);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(867, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 32);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Сортировка";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 567);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employessEntitiesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource employessEntitiesBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 23 день/try/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using @try;
+
+// ...
+
+namespace @try
+{
+    public partial class Form1 : Form
+    {
+        private employessEntities context; // Добавьте поле для хранения экземпляра контекста модели EDM
+
+        public Form1()
+        {
+            InitializeComponent();
+            context = new employessEntities(); // Создайте экземпляр контекста модели EDM
+            LoadDepartments(); // Загрузите список отделов при запуске формы
+        }
+
+        private void LoadDepartments()
+        {
+            var departments = context.Departments.ToList();
+            dataGridView1.DataSource = departments.ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Departments newDepartment = new Departments
+            {
+                department_name = txtName.Text,
+            };
+
+            context.Departments.Add(newDepartment);
+            context.SaveChanges();
+
+            LoadDepartments();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int departmentId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["department_id"].Value);
+                Departments department = context.Departments.Find(departmentId);
+
+                department.department_name = txtName.Text;
+
+                context.SaveChanges();
+                LoadDepartments();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int departmentId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["department_id"].Value);
+                Departments department = context.Departments.Find(departmentId);
+
+                context.Departments.Remove(department);
+                context.SaveChanges();
+
+                LoadDepartments();
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            // Сортировка по заданному критерию (например, по названию отдела)
+            var sortedDepartments = context.Departments.OrderBy(d => d.department_name).ToList();
+            dataGridView1.DataSource = sortedDepartments;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchText = txtSearch.Text;
+
+            // Поиск по двум полям (например, по названию отдела и другому полю)
+            var departments = context.Departments
+                .Where(d => d.department_name.Contains(searchText) || d.location.Contains(searchText))
+                .ToList();
+
+            dataGridView1.DataSource = departments;
+        }
+    }
+}
